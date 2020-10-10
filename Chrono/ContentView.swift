b15d420fc91d402ee0a2255dfd26ca21aa61d332
harmanchawla <hsc367@nyu.edu>
2020-10-10 01:04:42 -0400

    Initial Commit

---
+++ Chrono/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Chrono
+//
+//  Created by Harman Chawla on 10/10/20.
+//  Copyright © 2020 werkzeuge. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
